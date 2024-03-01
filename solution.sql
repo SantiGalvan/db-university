24b90f1724c67bcf51e9f7f0a4bb9a3c941c034a
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-01 21:18:48 +0100

    added fifth exercise

--- solution.sql
+++ solution.sql
@@ -9,3 +9,6 @@ SELECT * FROM `students` WHERE `date_of_birth` < '1995-01-01';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1;
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour` > '14:00:00';
