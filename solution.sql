230b339122f6d3c93eb6239f2327461b02c6cf31
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-02 09:55:55 +0100

    added second exercise (GROUP BY)

--- solution.sql
+++ solution.sql
@@ -29,3 +29,6 @@ SELECT COUNT(*) AS `n_teachers_phone` FROM `teachers` WHERE `phone` IS NOT NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 SELECT COUNT(*) AS `n_students_on_date`, `enrolment_date` FROM `students` GROUP BY `enrolment_date`;
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(*) AS `n_teachers`, `office_address` FROM `teachers` GROUP BY `office_address`;
