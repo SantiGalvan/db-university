6472ab88bcd385750e5d3ac16390e66f7941524e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-02 10:19:13 +0100

    added fourth exercise (GROUP BY)

--- solution.sql
+++ solution.sql
@@ -35,3 +35,6 @@ SELECT COUNT(*) AS `n_teachers`, `office_address` FROM `teachers` GROUP BY `offi
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 SELECT AVG(`vote`) AS `average_vote`, `exam_id` FROM `exam_student` GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(`department_id`) AS `n_courses` FROM `degrees` GROUP BY `department_id`;
